3608522d7b85d18b3bfab8a24eea024af10b85a1
Your Name <you@example.com>
2023-05-08 13:26:56 +0800

    Update README.md

--- movie-rating.sh
+++ movie-rating.sh
@@ -9,10 +9,12 @@ fi
 
 RATING=$(curl -s https://www.themoviedb.org/movie/${MOVIE_NAME}| grep data-percent | xargs -n1 |grep data-percent | awk -F = '{print $2}' | awk -F . '{print $1}')
 
+MOVIE_NAME=$(echo $MOVIE_NAME |  awk -F - '{print $2,$3,$4,$5,$6,$7,$8,$9,$10}')
+
 echo Movie Percentage is $RATING
 if [ "$RATING" -ge 70 ] ; then
-   echo MOVIE_NAME is good.
+   echo $MOVIE_NAME is good.
 else
-     echo MOVIE_NAME is Average.
+     echo $MOVIE_NAME is Average.
 
 fi
