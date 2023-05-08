1c0452b3e7004b260e57d6403668a721f3db39b3
Your Name <you@example.com>
2023-05-08 11:51:37 +0800

    Update README.md

--- 03-variables.sh
+++ 03-variables.sh
@@ -9,3 +9,10 @@ echo Today date is ${todayDate}
 ARTH=$((2+3*4/2))
 echo ARTH = ${ARTH}
 
+echo script Name - $0
+echo 1st Argiment - $1
+echo 2nd Argument - $2
+echo All Argument - $*
+echo No of Arguments - $#
+
+
